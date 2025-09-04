1728ae0543f8643a6ff94a001a2bbc46f3eb8cf9
Dami Johnson <dami@damijohnson.com>
2025-09-04 16:22:07 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -614,18 +614,18 @@ Devvit.addCustomPostType({
         <vstack gap="none" width="100%" height="100%" padding="none">
           {leaderboardData.length > 0 ? (
             <vstack width="100%" gap="none">
-              <hstack width="100%" padding="small">
-                <text size="small" width="15%" color={COLORS.NEUTRAL_400} weight="bold">RANK</text>
-                <text size="small" width="55%" color={COLORS.NEUTRAL_400} weight="bold">USER</text>
+              {/* <hstack width="100%" padding="small">
+                <text size="small" width="5%" color={COLORS.NEUTRAL_400} weight="bold">RANK</text>
+                <text size="small" width="65%" color={COLORS.NEUTRAL_400} weight="bold">USER</text>
                 <hstack width="30%" alignment="end">
                   <text size="small" color={COLORS.NEUTRAL_400} weight="bold">SCORE</text>
                 </hstack>
-              </hstack>
+              </hstack> */}
               {leaderboardData.map((entry, index) => (
                 <hstack key={entry.userId} width="100%" padding="small">
-                  <text size="medium" weight="bold" width="15%" color={COLORS.SECONDARY}>{index + 1}</text>
-                  <text size="medium" width="55%" color={COLORS.NEUTRAL_700}>u/{entry.userId}</text>
-                  <hstack width="30%" alignment="end">
+                  <text size="medium" weight="bold" width="5%" color={COLORS.SECONDARY}>{index + 1}.</text>
+                  <text size="medium" width="60%" color={COLORS.NEUTRAL_700}>u/{entry.userId}</text>
+                  <hstack width="35%" alignment="end">
                     <text size="medium" weight="bold" color={COLORS.PRIMARY}>${entry.score.toLocaleString()}</text>
                   </hstack>
                 </hstack>
